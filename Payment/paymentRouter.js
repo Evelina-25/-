88fb732b279df6666291ce38ee8842d09e695f74
Эвелина Ванярхина <evanyarkhina@inbox.ru>
2026-04-04 17:50:19 +0300

    изменила путь обновления

--- Payment/paymentRouter.js
+++ Payment/paymentRouter.js
@@ -1,11 +1,11 @@
-import Router from "express";
-import PaymentController from "./PaymentController.js";
-import authMiddleware from "../Middleware/authMiddleware.js";
+    import Router from "express";
+    import PaymentController from "./PaymentController.js";
+    import authMiddleware from "../Middleware/authMiddleware.js";
 
-const router = new Router();
+    const router = new Router();
 
-router.post("/", authMiddleware, PaymentController.create);
-router.get("/", authMiddleware, PaymentController.getAll);
-router.get("/:id", authMiddleware, PaymentController.getOne);
-router.patch('/pay/:id', authMiddleware, PaymentController.pay);
-export default router;
+    router.post("/", authMiddleware, PaymentController.create);
+    router.get("/", authMiddleware, PaymentController.getAll);
+    router.get("/:id", authMiddleware, PaymentController.getOne);
+    router.put('/:id', authMiddleware, PaymentController.pay);
+    export default router;
